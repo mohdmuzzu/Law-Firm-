0c1063d637e80ba2f163fe20ebc87c52cf392c63
Mohammed Muzammil <122904557+mohdmuzzu@users.noreply.github.com>
2023-12-03 16:54:42 +0530

    Add files via upload

---
+++ src/components/Introduction/Introduction.js
@@ -0,0 +1,24 @@
+import styles from "./Introduction.module.css";
+
+const Introduction = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.firstDiv}>
+        <p>
+          Let’s Introduce <br /> Ourself
+        </p>
+      </div>
+      <div className={styles.secondDiv}>
+        <h3>Criminal Lawyer</h3>
+        <p>
+          Amet minim mollit non deserunt ullamco est
+          <br /> sit aliqua dolor do amet sint. Velit officia consequatduis{" "}
+          <br />
+          enim velit mollit Exercitation.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Introduction;
